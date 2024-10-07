5587ea610b4979a2a5a71b3747a439750b63109f
victoracho <victoracho.box@gmail.com>
2024-10-07 16:29:40 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -144,7 +144,7 @@ onMounted(() => {
 <template>
   <b-row>
     <b-col cols="12" class="bg-secondary text-white col-xxl-2">
-      <side-nav msg="Daso Calendar">
+      <side-nav msg="Design Calendar">
         <template v-slot:content>
           <vue-date-picker v-model="date" inline auto-apply :enable-time-picker="false"
             @date-update="dateClicked"></vue-date-picker>
